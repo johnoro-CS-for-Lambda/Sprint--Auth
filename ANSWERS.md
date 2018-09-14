de7b7fa1f2f5a54a0a3289e83f3ecb836eca4b3f
johnoro <officialjohnorourke@gmail.com>
2018-09-14 11:25:04 -0400

    Completed answers.md and working on MVP.

--- ANSWERS.md
+++ ANSWERS.md
@@ -1,9 +1,11 @@
-<!-- Answers to the Short Answer Essay Questions go here -->
-
+<!-- Questions -->
 1. What is the purpose of using _sessions_?
-
 2. What does bcrypt do to help us store passwords in a secure manner.
-
 3. What does bcrypt do to slow down attackers?
-
 4. What are the three parts of the JSON Web Token?
+
+<!-- Answers -->
+1. Sessions are a way of storing specific data for a user. By way of a session ID, a user is given their current session data if there is any.
+2. Bcrypt allows us to hash passwords. It also allows us to give it a 'salt', which essentially will just help by adding that text onto our passwords, and then we also can add a cost, which is simply a number of iterations for running the hash.
+3. With the salt and the hash so many times over, it makes it so that it can be painstakingly slow for someone malicious to get ahold of a (plain-text) password, even if they manage to get all of your hashed passwords.
+4. There is a header, a payload, and a signature in a JWT.
--- config/routes.js
+++ config/routes.js
@@ -1,6 +1,8 @@
 const axios = require('axios');
-
+const bcrypt = require('bcryptjs');
+const db = require('../database/dbConfig');
 const { authenticate } = require('./middlewares');
+const secret = require('../_secrets/keys').jwtKey;
 
 module.exports = server => {
   server.post('/api/register', register);
@@ -8,12 +10,41 @@ module.exports = server => {
   server.get('/api/jokes', authenticate, getJokes);
 };
 
+function generateToken(id, name) {
+  const options = {
+    expiresIn: '1h',
+    subject: String(id)
+  };
+  return jwt.sign({ id, name }, secret, options);
+};
+
 function register(req, res) {
-  // implement user registration
+  let { username, password } = req.body;
+  if (!username || !password) {
+    res.status(404).json({ message: 'A username and password are required.' }).end();
+  }
+  password = bcrypt.hashSync(password, 10);
+  db('users').insert(req.body)
+    .then(([ id ]) => {
+      const token = generateToken(id, username);
+      res.status(201).json({ token });
+    })
+    .catch(err => res.status(500).json({ error: 'Registering error' }));
 }
 
 function login(req, res) {
-  // implement user login
+  const { username, password } = req.body;
+  if (!username || !password) {
+    res.status(404).json({ message: 'A username and password are required.' }).end();
+  }
+  db('users').where({ username }).first()
+    .then(user => {
+      if (!user || !bcrypt.compareSync(password, user.password)) {
+        res.status(401).json({ message: 'An incorrect username and/or password was provided.' }).end();
+      }
+      const token = generateToken(user.id, username);
+      res.status(200).json({ token });
+    })
 }
 
 function getJokes(req, res) {
